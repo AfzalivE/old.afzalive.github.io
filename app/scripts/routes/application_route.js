0e53a26814d621cd024c73ad571882bb664e1c40
Afzal Najam <afzalnaj@gmail.com>
2014-02-11 02:12:01 -0500

    Added header to the page
    Removed bootstrap

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,28 @@ EmberApp.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'blue'];
+        // return ['red', 'yellow', 'blue'];
+        return [
+            {
+                "name"  : "Twitter",
+                "class" : "icon-twitter",
+                "url"   : "http://www.twitter.com/AfzalivE"
+            },
+            {
+                "name"  : "Google Plus",
+                "class" : "icon-googleplus",
+                "url"   : "http://plus.google.com/+AfzalNajam"
+            },
+            {
+                "name"  : "GitHub",
+                "class" : "icon-github",
+                "url"   : "http://www.github.com/AfzalivE"
+            },
+            {
+                "name"  : "LinkedIn",
+                "class" : "icon-linkedin",
+                "url"   : "http://www.linkedin.com/in/AfzalivE"
+            }
+        ]
     }
 });
